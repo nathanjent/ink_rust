e86cd53598acb1d5fcedd5ae7039862e57a202de
nathanjent <nathanjent@gmail.com>
2016-06-22 06:12:19 -0400

    Trying to pass document reference into rendering update.

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,12 @@ extern crate find_folder;
 
 use std::fs::File;
 use std::io::Read;
+use std::sync::mpsc;
 
 use conrod::{Theme, Widget};
 use piston_window::*;
 
-use xml::{Parser, ElementBuilder};
+use xml::{Parser, Element, ElementBuilder};
 
 /// Conrod is backend agnostic. Here, we define the `piston_window` backend to use for our `Ui`.
 type Backend = (piston_window::G2dTexture<'static>, piston_window::Glyphs);
@@ -18,6 +19,27 @@ type Ui = conrod::Ui<Backend>;
 type UiCell<'a> = conrod::UiCell<'a, Backend>;
 
 // mod doc;
+
+struct InkApp {
+    document: Option<ElementBuilder>,
+
+        elem_sender: mpsc::Sender<(usize, usize, bool)>,
+            elem_receiver: mpsc::Receiver<(usize, usize, bool)>,
+}
+
+impl InkApp {
+    fn new() -> Self {
+        let (elem_sender, elem_receiver) = mpsc::channel();
+        InkApp { document: None, 
+            elem_sender: elem_sender,
+                        elem_receiver: elem_receiver,
+        }
+    }
+
+    fn setDocument(&mut self, document: ElementBuilder) {
+        self.document = Some(document);
+    }
+}
 
 fn main() {
 
@@ -33,20 +55,22 @@ fn main() {
 
     parser.feed_str(&file_string);
     let doc = parser.filter_map(|x| eb.handle_event(x));
+    let mut app = InkApp::new();
+    app.setDocument(eb);
 
     // Construct the window.
     let mut window: PistonWindow = WindowSettings::new("Inkrust", [720, 360])
-                                       .opengl(piston_window::OpenGL::V3_2)
-                                       .samples(4)
-                                       .exit_on_esc(true)
-                                       .build()
-                                       .unwrap();
+        .opengl(piston_window::OpenGL::V3_2)
+        .samples(4)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
 
     // Construct our `Ui`.
     let mut ui = {
         let assets = find_folder::Search::KidsThenParents(3, 5)
-                         .for_folder("assets")
-                         .unwrap();
+            .for_folder("assets")
+            .unwrap();
         let font_path = assets.join("fonts/NotoSans/NotoSans-Regular.ttf");
         let theme = Theme::default();
         let glyph_cache = piston_window::Glyphs::new(&font_path, window.factory.clone()).unwrap();
@@ -55,26 +79,21 @@ fn main() {
 
     window.set_ups(60);
 
-    for elem in doc {
-        let e = elem.unwrap();
-
-        println!("{}", e);
-    }
-
     // Poll events from the window.
     while let Some(event) = window.next() {
         ui.handle_event(event.clone());
-        event.update(|_| ui.set_widgets(set_ui));
+        event.update(|_| ui.set_widgets(|mut ui| set_widgets(&mut ui, &mut app)));
         window.draw_2d(&event, |c, g| ui.draw_if_changed(c, g));
     }
+}
 
-    // Declare the `WidgetId`s and instantiate the widgets.
-    fn set_ui(ref mut ui: UiCell) {
-        use conrod::{Canvas, Circle, Line, Oval, PointPath, Polygon, Positionable, Rectangle};
-        use std::iter::once;
+// Declare the `WidgetId`s and instantiate the widgets.
+fn set_widgets(ui: &mut UiCell, app: &mut InkApp) {
+    use conrod::{Canvas, Circle, Line, Oval, PointPath, Polygon, Positionable, Rectangle};
+    use std::iter::once;
 
-        // Generate a unique const `WidgetId` for each widget.
-        widget_ids!{
+    // Generate a unique const `WidgetId` for each widget.
+    widget_ids!{
 	        CANVAS,
 	        LINE,
 	        POINT_PATH,
@@ -86,32 +105,31 @@ fn main() {
 	        CIRCLE,
 	    };
 
-        // The background canvas upon which we'll place our widgets.
-        Canvas::new().pad(80.0).set(CANVAS, ui);
+    // The background canvas upon which we'll place our widgets.
+    Canvas::new().pad(80.0).set(CANVAS, ui);
 
-        Line::centred([-40.0, -40.0], [40.0, 40.0]).top_left_of(CANVAS).set(LINE, ui);
+    Line::centred([-40.0, -40.0], [40.0, 40.0]).top_left_of(CANVAS).set(LINE, ui);
 
-        let left = [-40.0, -40.0];
-        let top = [0.0, 40.0];
-        let right = [40.0, -40.0];
-        let points = once(left).chain(once(top)).chain(once(right));
-        PointPath::centred(points).down(80.0).set(POINT_PATH, ui);
+    let left = [-40.0, -40.0];
+    let top = [0.0, 40.0];
+    let right = [40.0, -40.0];
+    let points = once(left).chain(once(top)).chain(once(right));
+    PointPath::centred(points).down(80.0).set(POINT_PATH, ui);
 
-        Rectangle::fill([80.0, 80.0]).down(80.0).set(RECTANGLE_FILL, ui);
+    Rectangle::fill([80.0, 80.0]).down(80.0).set(RECTANGLE_FILL, ui);
 
-        Rectangle::outline([80.0, 80.0]).down(80.0).set(RECTANGLE_OUTLINE, ui);
+    Rectangle::outline([80.0, 80.0]).down(80.0).set(RECTANGLE_OUTLINE, ui);
 
-        let bl = [-40.0, -40.0];
-        let tl = [-20.0, 40.0];
-        let tr = [20.0, 40.0];
-        let br = [40.0, -40.0];
-        let points = once(bl).chain(once(tl)).chain(once(tr)).chain(once(br));
-        Polygon::centred_fill(points).right_from(LINE, 80.0).set(TRAPEZOID, ui);
+    let bl = [-40.0, -40.0];
+    let tl = [-20.0, 40.0];
+    let tr = [20.0, 40.0];
+    let br = [40.0, -40.0];
+    let points = once(bl).chain(once(tl)).chain(once(tr)).chain(once(br));
+    Polygon::centred_fill(points).right_from(LINE, 80.0).set(TRAPEZOID, ui);
 
-        Oval::fill([40.0, 80.0]).down(80.0).align_middle_x().set(OVAL_FILL, ui);
+    Oval::fill([40.0, 80.0]).down(80.0).align_middle_x().set(OVAL_FILL, ui);
 
-        Oval::outline([80.0, 40.0]).down(100.0).align_middle_x().set(OVAL_OUTLINE, ui);
+    Oval::outline([80.0, 40.0]).down(100.0).align_middle_x().set(OVAL_OUTLINE, ui);
 
-        Circle::fill(40.0).down(100.0).align_middle_x().set(CIRCLE, ui);
-    }
+    Circle::fill(40.0).down(100.0).align_middle_x().set(CIRCLE, ui);
 }
