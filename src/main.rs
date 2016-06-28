c43a227be5d15ab69a6a77a6a423274ed0a72df7
nathanjent <nathanjent@gmail.com>
2016-06-28 05:58:19 -0400

    Working on reading attributes for a rectangle from the DOM document.

--- src/main.rs
+++ src/main.rs
@@ -19,9 +19,9 @@ use piston_window::*;
 use lyon::path_builder::*;
 
 
-use xml5ever::tendril::{SliceExt};
-use xml5ever::{parse};
-use xml5ever::tree_builder::{TreeSink};
+use xml5ever::tendril::SliceExt;
+use xml5ever::parse;
+use xml5ever::tree_builder::TreeSink;
 use xml5ever::rcdom;
 use xml5ever::rcdom::{Document, Element, RcDom, Handle};
 
@@ -49,15 +49,15 @@ impl InkApp {
     fn set_dom(&mut self, dom: RcDom) {
         self.dom = dom;
     }
-    
+
     fn get_doc_handle(&mut self) -> Handle {
-    	self.dom.get_document()
+        self.dom.get_document()
     }
 }
 
 fn main() {
     let mut app = InkApp::new();
-    
+
     let mut file = File::open("tests/documents/testrect.svg").unwrap();
 
     let mut file_string = String::new();
@@ -66,24 +66,24 @@ fn main() {
         std::process::exit(1);
     };
 
-	let input = file_string.to_tendril();
+    let input = file_string.to_tendril();
 
     let dom: RcDom = parse(iter::once(input), Default::default());
     app.set_dom(dom);
 
     // Construct the window.
     let mut window: PistonWindow = WindowSettings::new("Inkrust", [720, 360])
-                                       .opengl(piston_window::OpenGL::V3_2)
-                                       .samples(4)
-                                       .exit_on_esc(true)
-                                       .build()
-                                       .unwrap();
+        .opengl(piston_window::OpenGL::V3_2)
+        .samples(4)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
 
     // Construct our `Ui`.
     let mut ui = {
         let assets = find_folder::Search::KidsThenParents(3, 5)
-                         .for_folder("assets")
-                         .unwrap();
+            .for_folder("assets")
+            .unwrap();
         let font_path = assets.join("fonts/NotoSans/NotoSans-Regular.ttf");
         let theme = Theme::default();
         let glyph_cache = piston_window::Glyphs::new(&font_path, window.factory.clone()).unwrap();
@@ -105,16 +105,14 @@ fn set_widgets(ui: &mut UiCell, app: &mut InkApp) {
     use conrod::{Canvas, Circle, Line, Oval, PointPath, Polygon, Positionable, Rectangle};
     use std::iter::once;
 
-	let doc = app.get_doc_handle();
-	walk("", doc);
-	
+    let doc = app.get_doc_handle();
+    walk("", ui, doc);
+
     // Generate a unique const `WidgetId` for each widget.
     widget_ids!{
 	        CANVAS,
 	        LINE,
 	        POINT_PATH,
-	        RECTANGLE_FILL,
-	        RECTANGLE_OUTLINE,
 	        TRAPEZOID,
 	        OVAL_FILL,
 	        OVAL_OUTLINE,
@@ -131,10 +129,6 @@ fn set_widgets(ui: &mut UiCell, app: &mut InkApp) {
     let right = [40.0, -40.0];
     let points = once(left).chain(once(top)).chain(once(right));
     PointPath::centred(points).down(80.0).set(POINT_PATH, ui);
-
-    Rectangle::fill([80.0, 80.0]).down(80.0).set(RECTANGLE_FILL, ui);
-
-    Rectangle::outline([80.0, 80.0]).down(80.0).set(RECTANGLE_OUTLINE, ui);
 
     let bl = [-40.0, -40.0];
     let tl = [-20.0, 40.0];
@@ -154,23 +148,37 @@ pub fn escape_default(s: &str) -> String {
     s.chars().flat_map(|c| c.escape_default()).collect()
 }
 
-fn walk(prefix: &str, handle: Handle) {
+fn walk(prefix: &str, ui: &mut UiCell, handle: Handle) {
+    use conrod::{Canvas, Circle, Line, Oval, PointPath, Polygon, Positionable, Rectangle};
     let node = handle.borrow();
 
     print!("{}", prefix);
     match node.node {
-        Document
-            => println!("#document"),
+        Document => println!("#document"),
 
-        rcdom::Text(ref text)  => {
-            println!("#text {}", escape_default(text))
-        },
+        rcdom::Text(ref text) => println!("#text {}", escape_default(text)),
 
-        Element(ref name, _) => {
+        Element(ref name, ref attr) => {
             println!("{}", name.local);
-        },
 
-        _ => {},
+            {
+                let map = attr.iter().map(|a| {
+                    (a.name, a.value)
+                });
+                for pair in map {
+                    println!("{:?}", pair);
+                }
+                widget_ids!{
+                    RECTANGLE_FILL,
+                    RECTANGLE_OUTLINE,
+                };
+                Rectangle::fill([80.0, 80.0]).down(80.0).set(RECTANGLE_FILL, ui);
+
+                Rectangle::outline([80.0, 80.0]).down(80.0).set(RECTANGLE_OUTLINE, ui);
+            }
+        }
+
+        _ => {}
 
     }
 
@@ -181,12 +189,12 @@ fn walk(prefix: &str, handle: Handle) {
         temp
     };
 
-    for child in node.children.iter()
+    for child in node.children
+        .iter()
         .filter(|child| match child.borrow().node {
-            rcdom::Text(_) | Element (_, _) => true,
+            rcdom::Text(_) | Element(_, _) => true,
             _ => false,
-        }
-    ) {
-        walk(&new_indent, child.clone());
+        }) {
+        walk(&new_indent, ui, child.clone());
     }
 }
