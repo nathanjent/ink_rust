6f5a97f920b73c29ccccd9b678d7d775f28b3530
nathanjent <nathanjent@gmail.com>
2016-06-28 22:50:13 -0400

    testing ideas for rendering shapes.

--- src/main.rs
+++ src/main.rs
@@ -102,55 +102,8 @@ fn main() {
 
 // Declare the `WidgetId`s and instantiate the widgets.
 fn set_widgets(ui: &mut UiCell, app: &mut InkApp) {
-    use conrod::{Canvas, Circle, Line, Oval, PointPath, Polygon, Positionable, Rectangle, XYPad};
-    use std::iter::once;
-
     let doc = app.get_doc_handle();
     walk("", ui, doc);
-
-    // Generate a unique const `WidgetId` for each widget.
-    widget_ids!{
-        CANVAS,
-        LINE,
-        POINT_PATH,
-        RECTANGLE_FILL,
-        RECTANGLE_OUTLINE,
-        TRAPEZOID,
-        OVAL_FILL,
-        OVAL_OUTLINE,
-        CIRCLE,
-        XYPAD,
-    };
-
-    // The background canvas upon which we'll place our widgets.
-    Canvas::new().pad(80.0).set(CANVAS, ui);
-
-    Line::centred([-40.0, -40.0], [40.0, 40.0]).top_left_of(CANVAS).set(LINE, ui);
-
-    let left = [-40.0, -40.0];
-    let top = [0.0, 40.0];
-    let right = [40.0, -40.0];
-    let points = once(left).chain(once(top)).chain(once(right));
-    PointPath::centred(points).down(80.0).set(POINT_PATH, ui);
-    
-    Rectangle::fill([80.0, 80.0]).down(80.0).set(RECTANGLE_FILL, ui);
-
-    Rectangle::outline([80.0, 80.0]).down(80.0).set(RECTANGLE_OUTLINE, ui);
-
-    let bl = [-40.0, -40.0];
-    let tl = [-20.0, 40.0];
-    let tr = [20.0, 40.0];
-    let br = [40.0, -40.0];
-    let points = once(bl).chain(once(tl)).chain(once(tr)).chain(once(br));
-    Polygon::centred_fill(points).right_from(LINE, 80.0).set(TRAPEZOID, ui);
-
-    Oval::fill([40.0, 80.0]).down(80.0).align_middle_x().set(OVAL_FILL, ui);
-
-    Oval::outline([80.0, 40.0]).down(100.0).align_middle_x().set(OVAL_OUTLINE, ui);
-
-    Circle::fill(40.0).down(100.0).align_middle_x().set(CIRCLE, ui);
-    
-//    XYPad::new(0.0, 0.0, 200.0, 0.0, 0.0, 200.0).w_h(150.0, 150.0).set(XYPAD, ui);
 }
 
 pub fn escape_default(s: &str) -> String {
@@ -158,6 +111,17 @@ pub fn escape_default(s: &str) -> String {
 }
 
 fn walk(prefix: &str, ui: &mut UiCell, handle: Handle) {
+    use conrod::{Canvas, Rectangle};
+    use std::iter::once;
+
+    widget_ids!{
+        CANVAS,
+        RECTANGLE_FILL,
+        RECTANGLE_OUTLINE,
+    };
+    
+    Canvas::new().pad(80.0).set(CANVAS, ui);
+
     let node = handle.borrow();
 
     print!("{}", prefix);
@@ -167,23 +131,27 @@ fn walk(prefix: &str, ui: &mut UiCell, handle: Handle) {
         rcdom::Text(ref text) => println!("#text {}", escape_default(text)),
 
         Element(ref name, ref attrs) => {
-        	let lname = name.local.as_ref();
+            let lname = name.local.as_ref();
             println!("{:?}", lname);
             match lname {
-            	"rect" => {
-            		let mut attr_it = attrs.into_iter();
-//            		let x = attr_it.find(|&a| a.name.local.as_ref() == "x");
-//            		let y = attr_it.find(|&a| a.name.local.as_ref() == "y");
-            		let width = attr_it.inspect(|&x| println!("{:?}", x)).find(|&a| a.name.local.as_ref() == "width");
-//            		let height = attr_it.find(|&a| a.name.local.as_ref() == "height");
-//            		let x = attr_it.find(|&a| a.name.local.as_ref() == "rx");
-//            		let y = attr_it.find(|&a| a.name.local.as_ref() == "ry");
-//                	println!("{:?}", x);
-//                	println!("{:?}", y);
-                	println!("{:?}", width);
-//                	println!("{:?}", height);
-            	}
-            	_ => {}
+                "rect" => {
+                    let mut attr_it = attrs.into_iter();
+                    let id = attr_it.inspect(|&x| println!("{:?}", x))
+                        .find(|&a| a.name.local.as_ref() == "id");
+                    //            		let x = attr_it.find(|&a| a.name.local.as_ref() == "x");
+                    //            		let y = attr_it.find(|&a| a.name.local.as_ref() == "y");
+                    // let width = attr_it.inspect(|&x| println!("{:?}", x))
+                    //    .find(|&a| a.name.local.as_ref() == "width");
+                    //            		let height = attr_it.find(|&a| a.name.local.as_ref() == "height");
+                    //            		let x = attr_it.find(|&a| a.name.local.as_ref() == "rx");
+                    //            		let y = attr_it.find(|&a| a.name.local.as_ref() == "ry");
+                    println!("{:?}", id);
+
+                    Rectangle::fill([80.0, 80.0]).down(80.0).set(id, ui);
+
+                    Rectangle::outline([80.0, 80.0]).down(80.0).set(id, ui);
+                }
+                _ => {}
             }
         }
         _ => {}
