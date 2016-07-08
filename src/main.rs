24f008d7760d548b5d2cd2a844834869ce3f8e63
Nathan Jent <n.jent@edaptive.com>
2016-07-08 16:44:07 -0400

    Added shape testing back in. Trying to parse style attribute.

--- src/main.rs
+++ src/main.rs
@@ -72,26 +72,31 @@ fn main() {
 
     // Construct the window.
     let mut window: PistonWindow = WindowSettings::new("Inkrust", [720, 360])
-        .opengl(piston_window::OpenGL::V3_2)
-        .samples(4)
-        .exit_on_esc(true)
-        .build()
-        .unwrap();
+                                       .opengl(piston_window::OpenGL::V3_2)
+                                       .samples(4)
+                                       .exit_on_esc(true)
+                                       .build()
+                                       .unwrap();
 
     // Construct our `Ui`.
     let mut ui = {
         let assets = find_folder::Search::KidsThenParents(3, 5)
-            .for_folder("assets")
-            .unwrap();
+                         .for_folder("assets")
+                         .unwrap();
         let font_path = assets.join("fonts/NotoSans/NotoSans-Regular.ttf");
         let theme = Theme::default();
         let glyph_cache = piston_window::Glyphs::new(&font_path, window.factory.clone()).unwrap();
         Ui::new(glyph_cache, theme)
     };
-    
+
     let mut app = InkApp::new();
     app.set_dom(dom);
-    
+
+    let doc = app.get_doc_handle();
+
+    // parse dom generating shapes for rendering
+    walk("", doc);
+
     window.set_ups(60);
 
     // Poll events from the window.
@@ -104,39 +109,62 @@ fn main() {
 
 // Declare the `WidgetId`s and instantiate the widgets.
 fn set_widgets(ui: &mut UiCell, app: &mut InkApp) {
-    use conrod::{
-    	Canvas, 
-    	Rectangle, 
-    	Positionable
-    };
+    use conrod::{Canvas, Circle, Line, Oval, PointPath, Polygon, Positionable, Rectangle};
+    use std::iter::once;
 
     widget_ids!{
         CANVAS,
+        LINE,
+        POINT_PATH,
         RECTANGLE_FILL,
         RECTANGLE_OUTLINE,
-    };
-    
-    Canvas::new().pad(80.0).set(CANVAS, ui);
-    
-    Rectangle::fill([40.0, 40.0]).down(40.0).set(RECTANGLE_FILL, ui);
+        TRAPEZOID,
+        OVAL_FILL,
+        OVAL_OUTLINE,
+        CIRCLE,
+	};
 
-    Rectangle::outline([40.0, 40.0]).down(40.0).set(RECTANGLE_OUTLINE, ui);
-    
-    let doc = app.get_doc_handle();
-    walk("", ui, doc);
+    // The background canvas upon which we'll place our widgets.
+    Canvas::new().pad(80.0).set(CANVAS, ui);
+
+    Line::centred([-40.0, -40.0], [40.0, 40.0]).top_left_of(CANVAS).set(LINE, ui);
+
+    let left = [-40.0, -40.0];
+    let top = [0.0, 40.0];
+    let right = [40.0, -40.0];
+    let points = once(left).chain(once(top)).chain(once(right));
+    PointPath::centred(points).down(80.0).set(POINT_PATH, ui);
+
+    Rectangle::fill([80.0, 80.0]).down(80.0).set(RECTANGLE_FILL, ui);
+
+    Rectangle::outline([80.0, 80.0]).down(80.0).set(RECTANGLE_OUTLINE, ui);
+
+    let bl = [-40.0, -40.0];
+    let tl = [-20.0, 40.0];
+    let tr = [20.0, 40.0];
+    let br = [40.0, -40.0];
+    let points = once(bl).chain(once(tl)).chain(once(tr)).chain(once(br));
+    Polygon::centred_fill(points).right_from(LINE, 80.0).set(TRAPEZOID, ui);
+
+    Oval::fill([40.0, 80.0]).down(80.0).align_middle_x().set(OVAL_FILL, ui);
+
+    Oval::outline([80.0, 40.0]).down(100.0).align_middle_x().set(OVAL_OUTLINE, ui);
+
+    Circle::fill(40.0).down(100.0).align_middle_x().set(CIRCLE, ui);
 }
 
 pub fn escape_default(s: &str) -> String {
     s.chars().flat_map(|c| c.escape_default()).collect()
 }
 
-fn walk(prefix: &str, ui: &mut UiCell, handle: Handle) {
-	use conrod::Rectangle;
-	use conrod::Dimensions;
-	use conrod::ShapeStyle as Style;
-	use conrod::color::Color;
-	use cssparser::{Parser, Token, Color as CSSParserColor};
-	
+fn walk(prefix: &str, handle: Handle) {
+    use conrod::Rectangle;
+    use conrod::Dimensions;
+    use conrod::ShapeStyle as Style;
+    use conrod::color::Color;
+    use cssparser::{Parser, DeclarationListParser, DeclarationParser, Token,
+                    Color as CSSParserColor, Delimiter};
+
     let node = handle.borrow();
 
     print!("{}", prefix);
@@ -150,64 +178,80 @@ fn walk(prefix: &str, ui: &mut UiCell, handle: Handle) {
             println!("{:?}", lname);
             match lname {
                 "rect" => {
-                	let mut dim: Dimensions = [0.0, 0.0];
-                	let mut style = Style::fill();
-                	for attr in attrs {
-                		let key_val = (attr.name.local.as_ref(), attr.value.as_ref());
-                		match key_val {
-							w @ ("width", _) => {
-                				let (_, w) = w;
-			        			let w = w.parse::<f64>()
-			            			.expect("Parse error");
-		            			dim[0] = w;
-			                 	println!("{:?}", w);
-							}
-                			h @ ("height", _) => {
-                				let (_, h) = h;
-			        			let h = h.parse::<f64>()
-			            			.expect("Parse error");
-		            			dim[1] = h;
-			                    println!("{:?}", h);
-                			}
-                			i @ ("id", _) => {
-                				let (_, i) = i;
-                				println!("{:?}", i);
-                			}
-							s @ ("style", _) => {
-                				let (_, s) = s;
-                				let mut parser = Parser::new(&s);
-                				while !parser.is_exhausted() {
-									let token = parser
-									.next_including_whitespace_and_comments()
-									.expect("Style parsing error.");
-									match token {
-										Token::Ident(id) => println!("{:?} id", id),
-										Token::Hash(s) | Token::IDHash(s) => {
-											println!("{:?} i", s);
-										}
-										Token::AtKeyword(s) => println!("{:?} k", s),
-										Token::QuotedString(s) => println!("{:?} q", s),
-										Token::UnquotedUrl(s) => println!("{:?} u", s),
-										Token::Number(s) => println!("{:?} n", s),
-										Token::UnicodeRange(v, s) => println!("{:?}{:?} r", v, s),
-										Token::WhiteSpace(s) => println!("{:?} w", s),
-										Token::Dimension(v, s) => println!("{:?}{:?} d", v, s),
-										_ => { }
-									}
-//									let color = CSSParserColor::parse(&mut Parser::new(&s))
-//									.expect("Color parse error");
-//        							style.set_color(Color::Rgba(
-//        									color.red,
-//        									color.green,
-//        									color.blue,
-//        									color.alpha));
-                				}
-			                    println!("{:?}", s);
-							}
-							_ => {}
-                		}
-                	}
-                	let rect = Rectangle::styled(dim, style);
+                    let mut dim: Dimensions = [0.0, 0.0];
+                    let mut style = Style::fill();
+                    for attr in attrs {
+                        let key_val = (attr.name.local.as_ref(), attr.value.as_ref());
+                        match key_val {
+                            w @ ("width", _) => {
+                                let (_, w) = w;
+                                let w = w.parse::<f64>()
+                                         .expect("Parse error");
+                                dim[0] = w;
+                                println!("{:?}", w);
+                            }
+                            h @ ("height", _) => {
+                                let (_, h) = h;
+                                let h = h.parse::<f64>()
+                                         .expect("Parse error");
+                                dim[1] = h;
+                                println!("{:?}", h);
+                            }
+                            i @ ("id", _) => {
+                                let (_, i) = i;
+                                println!("{:?}", i);
+                            }
+                            s @ ("style", _) => {
+                                let (_, s) = s;
+                                println!("{:?}", s);
+                                //      let mut parser = Parser::new(&s);
+                                //    let property = DeclarationListParser::new(
+                                //    	&mut parser,
+                                //    	DeclarationParser)
+                                //    .map(|result| {
+                                // 	            result.unwrap()
+                                // 			}).collect();
+                                //    println!("{:?}", property);
+                                // 	while !parser.is_exhausted() {
+                                // 		// TODO parse property string into (key,value)
+                                // 				let property = parser.parse_until_before(
+                                // 					Delimiter::Semicolon | Delimiter::None,
+                                // 					|mut t| loop {
+                                // 						match t.next_including_whitespace_and_comments() {
+                                // 					Ok(Token::WhiteSpace(_)) |
+                                // 					Ok(Token::Colon) |
+                                // 					Ok(Token::Comment(_)) => {},
+                                // 			                r => return r
+                                // 						}
+                                // 			        }
+                                // 				).expect("Style parse error");
+                                // 				match property {
+                                // 					(Token::Ident(k), Token::Hash(v)) |
+                                // 						(Token::Ident(k), Token::IDHash(v)) => {
+                                // 								println!("{:?}:{:?}", k, v);
+                                // 								let color = CSSParserColor::parse(&mut Parser::new(&s))
+                                // 								.expect("Color parse error");
+                                // 							style.set_color(Color::Rgba(
+                                // 									color.red,
+                                // 									color.green,
+                                // 									color.blue,
+                                // 									color.alpha));
+                                // 					}
+                                // 					Token::AtKeyword(s) => println!("{:?} k", s),
+                                // 					Token::QuotedString(s) => println!("{:?} q", s),
+                                // 					Token::UnquotedUrl(s) => println!("{:?} u", s),
+                                // 					Token::Number(s) => println!("{:?} n", s),
+                                // 					Token::UnicodeRange(v, s) => println!("{:?}{:?} r", v, s),
+                                // 					Token::WhiteSpace(s) => println!("{:?} w", s),
+                                // 					Token::Dimension(v, s) => println!("{:?}{:?} d", v, s),
+                                // 					_ => { }
+                                // 				}
+                                // 	}
+                            }
+                            _ => {}
+                        }
+                    }
+                    let rect = Rectangle::styled(dim, style);
                 }
                 _ => {}
             }
@@ -223,11 +267,11 @@ fn walk(prefix: &str, ui: &mut UiCell, handle: Handle) {
     };
 
     for child in node.children
-        .iter()
-        .filter(|child| match child.borrow().node {
-            rcdom::Text(_) | Element(_, _) => true,
-            _ => false,
-        }) {
-        walk(&new_indent, ui, child.clone());
+                     .iter()
+                     .filter(|child| match child.borrow().node {
+                         rcdom::Text(_) | Element(_, _) => true,
+                         _ => false,
+                     }) {
+        walk(&new_indent, child.clone());
     }
 }
