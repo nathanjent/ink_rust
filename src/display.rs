9c4fd1a1de35f2264bb4ece415da15cfd03cc870
nathanjent <nathanjent@gmail.com>
2017-02-06 21:38:30 -0500

    Update to get UI test running.

--- src/display.rs
+++ src/display.rs
@@ -1,58 +1,54 @@
 use inkapp::InkApp;
+use conrod;
+use conrod::{widget, Colorable, Positionable, Sizeable, Widget};
+use conrod::backend::glium::glium;
+use conrod::backend::glium::glium::{DisplayBuild, Surface};
+use find_folder;
+use std;
 
-#[cfg(not(all(feature="winit", feature="glium")))]
-pub fn load(app: &mut InkApp) {}
-
-#[cfg(all(feature="winit", feature="glium"))]
 pub fn load(app: &mut InkApp) {
-    use conrod;
-    use conrod::{widget, Colorable, Positionable, Sizeable, Widget};
-    use conrod::backend::glium::glium;
-    use conrod::backend::glium::glium::{DisplayBuild, Surface};
-    use find_folder;
-    use std;
+    const WIDTH: u32 = 400;
+    const HEIGHT: u32 = 200;
 
-    const WIDTH: u32 = 720;
-    const HEIGHT: u32 = 360;
-
+    // Build the window.
     let display = glium::glutin::WindowBuilder::new()
         .with_vsync()
         .with_dimensions(WIDTH, HEIGHT)
-        .with_title("Inkrust")
+        .with_title("Hello Conrod!")
         .build_glium()
         .unwrap();
 
+    // construct our `Ui`.
     let mut ui = conrod::UiBuilder::new([WIDTH as f64, HEIGHT as f64]).build();
 
     // Generate the widget identifiers.
-    widget_ids!{
-        struct Ids {
-            text,
-            background,
-        }
-    }
+    widget_ids!(struct Ids { text });
     let ids = Ids::new(ui.widget_id_generator());
 
     // Add a `Font` to the `Ui`'s `font::Map` from file.
-    let assets = find_folder::Search::KidsThenParents(3, 5).for_folder("assets").unwrap();
+    let assets
+        = find_folder::Search::KidsThenParents(3, 5).for_folder("assets").unwrap();
     let font_path = assets.join("fonts/NotoSans/NotoSans-Regular.ttf");
     let regular = ui.fonts.insert_from_file(font_path).unwrap();
 
-    // A type used for converting `conrod::render::Primitives` into `Command`s that can be used
-    // for drawing to the glium `Surface`.
-    let mut renderer = conrod::backend::glium::Renderer::new(&display).unwrap();
+        // A type used for converting `conrod::render::Primitives` into `Command`s
+        // that can be used for drawing to the glium `Surface`.
+        let mut renderer = conrod::backend::glium::Renderer::new(&display).unwrap();
 
-    // The image map describing each of our widget->image mappings (in our case, none).
-    let image_map = conrod::image::Map::new();
+        // The image map describing each of our widget->image mappings
+        // (in our case, none).
+        let image_map = conrod::image::Map::<glium::texture::Texture2d>::new();
 
-    // Poll events from the window.
-    let mut last_update = std::time::Instant::now();
-    let mut ui_needs_update = true;
-    'main: loop {
-        // We don't want to loop any faster than 60 FPS, so wait until it has been at least
-        // 16ms since the last yield.
+        // Poll events from the window.
+        let mut last_update = std::time::Instant::now();
+        let mut ui_needs_update = true;
+        'main: loop {
+
+        // We don't want to loop any faster than 60 FPS,
+        // so wait until it has been at least 16ms since the last yield.
         let sixteen_ms = std::time::Duration::from_millis(16);
-        let duration_since_last_update = std::time::Instant::now().duration_since(last_update);
+        let duration_since_last_update
+            = std::time::Instant::now().duration_since(last_update);
         if duration_since_last_update < sixteen_ms {
             std::thread::sleep(sixteen_ms - duration_since_last_update);
         }
@@ -60,7 +56,8 @@ pub fn load(app: &mut InkApp) {
         // Collect all pending events.
         let mut events: Vec<_> = display.poll_events().collect();
 
-        // If there are no events and the `Ui` does not need updating, wait for the next event.
+        // If there are no events and the `Ui` does not need updating,
+        // wait for the next event.
         if events.is_empty() && !ui_needs_update {
             events.extend(display.wait_events().next());
         }
@@ -81,8 +78,9 @@ pub fn load(app: &mut InkApp) {
             match event {
                 // Break from the loop upon `Escape`.
                 glium::glutin::Event::KeyboardInput(_, _, Some(glium::glutin::VirtualKeyCode::Escape)) |
-                    glium::glutin::Event::Closed => break 'main,
-                _ => {}
+                glium::glutin::Event::Closed =>
+                    break 'main,
+                _ => {},
             }
         }
 
